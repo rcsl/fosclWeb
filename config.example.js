6d94c27c3986e0afc611c310338a023ce41e6883
mike jones <rcsl@users.noreply.github.com>
2017-06-07 16:30:40 +0100

    version0.2

--- config.example.js
+++ config.example.js
@@ -3,14 +3,15 @@ config.smtp ={
   user : 'no-reply@friendsofsonningcommonlibrary.org.uk',
   password : 'secret'
 };
-config.email = "readingcomputersolutions@gmail.com";
+config.default ={};
+config.default.email = "readingcomputersolutions@gmail.com";
 
 config.contactUs = [
-        { "reason":"Membership", "email":"membership@friendsofsonningcommonlibrary.org.uk" },
-        { "reason":"Volunteering",  "email": "rota@friendsofsonningcommonlibrary.org.uk" },
-        { "reason":"General Question", "email": "secretary@friendsofsonningcommonlibrary.org.uk" },
-        { "reason":"Web-site",  "email": "media@friendsofsonningcommonlibrary.org.uk" },
-        { "reason":"Other", "email": "secretary@friendsofsonningcommonlibrary.org.uk" },
+        { "id" : 0, "reason":"About Joining FoSCL", "email":"membership@friendsofsonningcommonlibrary.org.uk" },
+        { "id" : 1, "reason":"About Volunteering",  "email": "rota@friendsofsonningcommonlibrary.org.uk" },
+        { "id" : 2, "reason":"Just a General Question", "email": "secretary@friendsofsonningcommonlibrary.org.uk" },
+        { "id" : 3, "reason":"About the Web-site",  "email": "media@friendsofsonningcommonlibrary.org.uk" },
+        { "id" : 4, "reason":"Something else", "email": "secretary@friendsofsonningcommonlibrary.org.uk" },
     ];
 
 
