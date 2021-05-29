0979db864be3671cf7aa096de876cd0c9aa35f9d
rcsl <rcsl@users.noreply.github.com>
2021-05-29 12:59:51 +0100

    redo

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-const { body, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 var helmet = require('helmet');
 
